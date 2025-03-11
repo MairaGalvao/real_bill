723b8270da902f5bbd59c169e3f6feca35b3bd5e
Maira Galvao <mairagalvao01@gmail.com>
2025-03-11 16:43:02 +0200

    server folder deleted

--- myapp/views.py
+++ myapp/views.py
@@ -5,7 +5,7 @@ class PlantList(APIView):
     def get(self, request):
         # Fake data (replace with real data from DB later)
         fake_plants = [
-            {"id": 1, "name": "alo", "price": "10.00"},
+            {"id": 1, "name": "dasdsadas", "price": "10.00"},
             {"id": 2, "name": "Tulip", "price": "5.50"},
             {"id": 3, "name": "Sunflower", "price": "7.20"}
         ]
